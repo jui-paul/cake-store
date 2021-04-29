a9969710824077c6bac0459b9c3c716ffd8fd4a6
jui-paul <juipaul459@gmail.com>
2021-04-30 02:29:29 +0600

    signup form

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+// Get the modal
+var modal2 = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
